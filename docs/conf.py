05739130146c32a201c6d36f3c3ee89e7bd308f8
Ashwin Joshi <ashwin.joshi@keysight.com>
2025-05-31 09:03:33 -0700

    Resolved Dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+autodoc_mock_imports = ["cyperf", "pandas"]
+
 project = 'CyPerf REST Python Client'
 copyright = '2024'
 author = 'CyPerf Team'
